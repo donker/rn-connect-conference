45442484579588b28f2d161af434381c56615ef0
Peter Donker <peter@bring2mind.net>
2019-03-13 22:40:26 +0100

    Fix for Android

--- App.tsx
+++ App.tsx
@@ -2,9 +2,33 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./src/store";
 import AppContainer from "./src/routing/AppContainer";
+import { Font } from "expo";
+import LoadScreen from "./src/screens/components/LoadScreen";
 
-export default class App extends React.Component {
+interface IState {
+  loading: boolean;
+}
+
+export default class App extends React.Component<{}, IState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ loading: false });
+  }
+
   render() {
+    if (this.state.loading) {
+      return <LoadScreen />;
+    }
     return (
       <Provider store={store}>
         <AppContainer />
