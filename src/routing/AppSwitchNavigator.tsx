f6ed5142558f3fb180a6d1a140e867c2987cf36e
Peter Donker <peter@bring2mind.net>
2019-03-21 14:47:34 +0100

    Fixes to data persistence

--- src/routing/AppSwitchNavigator.tsx
+++ src/routing/AppSwitchNavigator.tsx
@@ -1,53 +1,10 @@
 import * as React from "react";
-import { createSwitchNavigator, NavigationScreenProps } from "react-navigation";
+import { createSwitchNavigator } from "react-navigation";
 import ScanStack from "./ScanStack";
 import LoadConfScreen from "../screens/LoadConfScreen";
 import Conference from "./Conference";
-import { IAppState } from "../models";
-import { AsyncStorage } from "react-native";
-import { setConference, refreshAttendances } from "../actions/appActions";
-import LoadScreen from "../screens/components/LoadScreen";
+import SwitchScreen from '../screens/SwitchScreen';
 
-interface IStateProps {
-  appState: IAppState;
-}
-interface IDispatchProps {
-  setConference: typeof setConference;
-  refreshAttendances: typeof refreshAttendances;
-}
-interface IProps extends IStateProps, IDispatchProps, NavigationScreenProps {}
-
-class SwitchScreen extends React.Component<IProps> {
-  componentDidMount() {
-    AsyncStorage.getItem("conference")
-      .then(value => {
-        if (value) {
-          this.props.setConference(JSON.parse(value));
-        }
-      })
-      .then(() => {
-        if (this.props.appState.conference.ConferenceId == -1) {
-          // goto scan page
-          this.props.navigation.navigate("Scan");
-        } else {
-          // goto conference page
-          if (this.props.appState.network) {
-            this.props.refreshAttendances(
-              this.props.appState.conference.Site,
-              this.props.appState.conference.ConferenceId
-            );
-          }
-          this.props.navigation.navigate("Conference");
-        }
-      })
-      .catch(() => {
-        this.props.navigation.navigate("Scan");
-      });
-  }
-  render() {
-    return <LoadScreen />;
-  }
-}
 const AppSwitchNavigator = createSwitchNavigator({
   Switch: { screen: SwitchScreen },
   Scan: { screen: ScanStack },
---
+++ src/screens/SwitchScreen.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import { NavigationScreenProps } from "react-navigation";
+import { bindActionCreators, Dispatch } from "redux";
+import { IAppState } from "../models";
+import { AsyncStorage } from "react-native";
+import { setConference, refreshAttendances } from "../actions/appActions";
+import LoadScreen from "../screens/components/LoadScreen";
+import { connect } from "react-redux";
+import { IRootState } from "../models/state/state";
+
+interface IStateProps {
+  appState: IAppState;
+}
+interface IDispatchProps {
+  setConference: typeof setConference;
+  refreshAttendances: typeof refreshAttendances;
+}
+interface IProps extends IStateProps, IDispatchProps, NavigationScreenProps {}
+
+class SwitchScreen extends React.Component<IProps> {
+  componentDidMount() {
+    AsyncStorage.getItem("conference")
+      .then(value => {
+        if (value) {
+          console.log("data", value);
+          this.props.setConference(JSON.parse(value));
+        }
+      })
+      .then(() => {
+        if (this.props.appState.conference.ConferenceId == -1) {
+          // goto scan page
+          this.props.navigation.navigate("Scan");
+        } else {
+          // goto conference page
+          if (this.props.appState.network) {
+            this.props.refreshAttendances(
+              this.props.appState.conference.Site,
+              this.props.appState.conference.ConferenceId
+            );
+          }
+          this.props.navigation.navigate("Conference");
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        this.props.navigation.navigate("Scan");
+      });
+  }
+  render() {
+    return <LoadScreen />;
+  }
+}
+
+export default connect(
+  (state: IRootState): IStateProps => {
+    return {
+      appState: state.app
+    };
+  },
+  (dispatch: Dispatch) =>
+    bindActionCreators(
+      {
+        setConference,
+        refreshAttendances
+      },
+      dispatch
+    )
+)(SwitchScreen);
